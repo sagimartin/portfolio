b14efde3631c9580763e4b2f4dfe5f31ced8eb4c
Martin Sági <sagimrtin@gmail.com>
2023-08-13 21:06:29 +0200

    Add Navbar component with basic styling &  SVG icons

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,10 +1,14 @@
+import { Container } from '@mui/material'
+import Navbar from './components/Navbar'
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <Container style={{}}>
+      <Navbar />
+    </Container>
   )
 }
 
---
+++ Portfolio/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { AppBar, Box, Typography } from "@mui/material";
+import { useState } from 'react';
+
+import html from "../assets/icons/html.svg"
+import glyph from "../assets/icons/glyph.svg"
+
+
+export default function Navbar() {
+    const [showGlyph, setShowGlyph] = useState(false)
+    return (
+        <AppBar className="navbar" elevation={{}} sx={{ width: "100%", display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", backgroundColor: "white", color: "black" }}>
+            <Box className="intro" sx={{ display: "flex", flexDirection: "column" }}>
+                <Typography variant="h1" color={"#290BA2"} marginBottom={"1.5rem"} style={{
+                    fontFamily: "var(--secondary-font)",
+                    fontWeight: "600"
+                }}>
+                    Hello!
+                </Typography>
+                <Typography variant="h3" textAlign={"justify"} style={{ fontFamily: "var(--secondary-font)" }}>
+                    I'm <span style={{ backgroundColor: 'yellow', padding: ".2rem" }}><strong>Martin.</strong></span>*
+                </Typography>
+                <Typography className="introSub" variant='h6' style={{ display: "flex", alignItems: "center", justifyContent: "center", fontFamily: "var(--secondary-font)", marginTop: ".25rem" }} >
+                    * a frontend developer based in Budapest.
+                </Typography>
+                <img className="rollingHTML" src={showGlyph ? glyph : html} alt="logo" style={{ width: "2.5rem", margin: "0 .5rem" }} onMouseEnter={() => setShowGlyph(true)} onMouseLeave={() => setShowGlyph(false)} />
+            </Box>
+            <Box className="menuButtons" style={{ display: "flex", flexDirection: "column", gap: ".5rem", width: "max-content" }}>
+                <Typography variant='h6' className="menuButton">{'<Home/>'}</Typography>
+                <Typography variant='h6' className="menuButton">{'<Skills/>'}</Typography>
+                <Typography variant='h6' className="menuButton">{'<Projects/>'}</Typography>
+                <Typography variant='h6' className="menuButton">{'<About/>'}</Typography>
+            </Box>
+        </AppBar>
+    )
+}
