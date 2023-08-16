9f218d4accf25ed68a775c6e7ebc162ae8fdec3b
Martin Sági <sagimrtin@gmail.com>
2023-08-16 09:12:25 +0200

    Add initializePositions function to generate random positions for popups

--- Portfolio/src/components/about/AboutPopup.jsx
+++ Portfolio/src/components/about/AboutPopup.jsx
@@ -2,28 +2,41 @@ import { Button } from '@mui/base';
 import { Box, Typography } from '@mui/material';
 import { useState } from 'react';
 import Draggable from 'react-draggable';
-
 import './AboutPopup.css';
+
+// Initialize positions for popups
+const initializePositions = (activePopups, popupWidth, popupHeight) => {
+  return activePopups.map(() => ({
+    x: Math.floor(Math.random() * (window.innerWidth - popupWidth)),
+    y: Math.floor(Math.random() * (window.innerHeight - popupHeight)),
+  }));
+};
 
 export default function AboutPopup({ memes, closePopup }) {
   const [activePopups, setActivePopups] = useState(memes.filter(meme => !meme.hide));
-  const initialPositions = activePopups.map(() => getRandomPosition(300, 400));
-  const [positions, setPositions] = useState(initialPositions);
+  const [positions, setPositions] = useState(() => initializePositions(activePopups, 300, 400));
   const [showReadme, setShowReadme] = useState(true);
 
+  // Handle close README
   const handleCloseReadme = () => {
     setShowReadme(false);
     if (activePopups.length === 0) closePopup();
   };
 
+  // Handle close individual popup
   const handleClosePopup = (index) => {
-    const newActivePopups = activePopups.filter((_, i) => i !== index);
+    const newActivePopups = [...activePopups];
+    newActivePopups.splice(index, 1);
     setActivePopups(newActivePopups);
-    const newPositions = positions.filter((_, i) => i !== index);
+
+    // Generate new positions for remaining popups
+    const newPositions = initializePositions(newActivePopups, 300, 400);
     setPositions(newPositions);
+
     if (newActivePopups.length === 0 && !showReadme) closePopup();
   };
 
+  // Handle close all popups
   const handleCloseAll = () => {
     closePopup();
     setActivePopups([]);
@@ -31,15 +44,17 @@ export default function AboutPopup({ memes, closePopup }) {
     setShowReadme(false);
   };
 
-  function getRandomPosition(popupWidth, popupHeight) {
-    const x = Math.floor(Math.random() * (window.innerWidth - popupWidth));
-    const y = Math.floor(Math.random() * (window.innerHeight - popupHeight));
-    return { x, y };
-  }
+  // Handle drag event for popup
+  const handleDrag = (index, newPosition) => {
+    const newPositions = [...positions];
+    newPositions[index] = newPosition;
+    setPositions(newPositions);
+  };
 
   return (
     <Box className="popup-overlay">
       {showReadme && (
+        // README Popup
         <Draggable handle=".drag-handle-readme" bounds={{ top: 0, left: 0, right: window.innerWidth, bottom: window.innerHeight }}
           defaultPosition={{ x: '50%', y: '50%' }}>
           <Box className="popup readme drag-handle-readme">
@@ -47,12 +62,13 @@ export default function AboutPopup({ memes, closePopup }) {
               <Typography>README.md</Typography>
               <Button className="close-button" onClick={handleCloseReadme}>✕</Button>
             </Box>
-            <Typography className='readme-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores reiciendis saepe sed, ex rem iure corporis sunt nihil atque velit asperiores veniam nesciunt dignissimos reprehenderit laboriosam quia et architecto optio.</Typography>
+            <Typography className='readme-text'>Lorem ipsum, dolor sit amet consectetur adipisicing elit...</Typography>
           </Box>
         </Draggable>
       )}
       {activePopups.map((meme, index) => (
-        <Draggable key={index} handle=".drag-handle" defaultPosition={positions[index]} bounds="parent">
+        // Meme Popups
+        <Draggable key={index} handle=".drag-handle" position={positions[index]} bounds={{ top: 0, left: 0, right: window.innerWidth, bottom: window.innerHeight }} onDrag={(_, newPosition) => handleDrag(index, newPosition)}>
           <Box className="popup drag-handle">
             <Box className="popup-header">
               <Typography>{meme.title}</Typography>
@@ -64,7 +80,7 @@ export default function AboutPopup({ memes, closePopup }) {
           </Box>
         </Draggable>
       ))}
-      <Button className='popup close-all' onClick={handleCloseAll}>Close All ✕ </Button>
+      <Button className='popup close-all' onClick={handleCloseAll}>Close All ✕</Button>
     </Box>
   );
 }
