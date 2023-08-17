c7fa1019046932243fc3a0faf6729cb57af18d19
Martin Sági <sagimrtin@gmail.com>
2023-08-17 21:49:54 +0200

    Update Container component with max and min width

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
 
   return (
-    <Container style={{}}>
+    <Container style={{maxWidth: "1440px", minWidth: "320px"}}>
       <Navbar />
     </Container>
   )
