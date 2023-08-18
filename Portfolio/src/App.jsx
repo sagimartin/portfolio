ca855535a6ddf8c527c32e443186d8ccea79210e
Martin Sági <sagimrtin@gmail.com>
2023-08-18 22:16:34 +0200

    Implement basic Skills component

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,15 +1,15 @@
-import { Container } from '@mui/material'
-import Navbar from './components/navbar/Navbar'
-
-import './App.css'
+import { Container } from '@mui/material';
+import Navbar from './components/navbar/Navbar';
+import Skills from './components/skills/Skills';
+import './App.css';
 
 function App() {
-
   return (
-    <Container style={{maxWidth: "1440px", minWidth: "320px"}}>
+    <Container style={{ maxWidth: "1440px", minWidth: "320px" }}>
       <Navbar />
+      <Skills />
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Portfolio/src/components/skills/Skills.jsx
@@ -0,0 +1,18 @@
+import { Box, Typography } from "@mui/material";
+
+import "./Skills.css"
+
+export default function Skills() {
+    return (
+        <Box className="window-frame">
+            <Box className="header">
+                <Box>{'<Skills/>'}</Box>
+                <Box>_ ▢ X</Box>
+                </Box>
+            <Typography>
+            halllo
+            </Typography>
+        </Box>
+    );
+  }
+  
