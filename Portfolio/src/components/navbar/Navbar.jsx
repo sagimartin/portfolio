9108dc77ebceca1bb05d1a6900f6e87c5c38e5a9
Martin Sági <sagimrtin@gmail.com>
2023-08-20 19:45:01 +0200

    Add icon animation

--- Portfolio/src/components/navbar/Navbar.jsx
+++ Portfolio/src/components/navbar/Navbar.jsx
@@ -1,12 +1,14 @@
 import { AppBar, Box } from "@mui/material";
 import { useState } from 'react';
 import AboutPopup from "../about/AboutPopup";
-import aboutData from "../../data/about.json"
-import hello from "/assets/images/about/hello.svg"
-import about from "../../../public/assets/icons/about.svg"
+import aboutData from "../../data/about.json";
+import hello from "/assets/images/about/hello.svg";
+import about from "../../../public/assets/icons/about.svg";
+
+import "./animations.css"
 
 export default function Navbar() {
-    const [showAboutPopup, setShowAboutPopup] = useState(false)
+    const [showAboutPopup, setShowAboutPopup] = useState(false);
     const closeAboutPopup = () => {
         setShowAboutPopup(false);
     };
@@ -17,17 +19,28 @@ export default function Navbar() {
             boxShadow: "none",
             position: "sticky"
         }}>
-            <img src={hello} alt="Hello" style={{
-                width: "100%",
-                margin: "0 auto"
-            }} />
+            <img
+                src={hello}
+                alt="Hello"
+                style={{
+                    width: "100%",
+                    margin: "0 auto",
+                }}
+            />
             <Box sx={{
                 cursor: "pointer",
                 position: "absolute",
                 top: "2rem",
-                right: "10rem", 
+                right: "10rem",
             }} >
-                <img src={about} alt="About me" style={{width: "10rem"}} onClick={() => setShowAboutPopup(true)} />
+                <img
+                    src={about}
+                    alt="About me"
+                    style={{
+                        width: "10rem", animation: "pulse 2s infinite"
+                    }}
+                    onClick={() => setShowAboutPopup(true)}
+                />
                 {showAboutPopup && <AboutPopup memes={aboutData.memes} closePopup={closeAboutPopup} />}
             </Box>
         </AppBar>
