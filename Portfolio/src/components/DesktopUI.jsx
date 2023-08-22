88c874b264bd5a541d2e8629ee676bcf66fa2a36
Martin Sági <sagimrtin@gmail.com>
2023-08-22 15:05:17 +0200

    Implement conditional rendering

--- Portfolio/src/components/DesktopUI.jsx
+++ Portfolio/src/components/DesktopUI.jsx
@@ -4,6 +4,9 @@ import Footer from "./footer/Footer";
 
 import AboutPopup from "./about/AboutPopup";
 import aboutData from "../data/about.json";
+import Contacts from "./contacts/Contacts"
+import Projects from "./projects/Projects"
+import Skills from "./skills/Skills"
 
 import hello from "/assets/images/about/hello.svg";
 import contactsIcon from "/assets/icons/contacts.svg";
@@ -14,7 +17,11 @@ import aboutIcon from "/assets/icons/about.svg";
 import "./DesktopUI.css";
 
 export default function DesktopUI() {
+    const [showContacts, setShowContacts] = useState(false)
+    const [showProjects, setShowProjects] = useState(false)
+    const [showSkills, setShowSkills] = useState(false)
     const [showAboutPopup, setShowAboutPopup] = useState(false);
+
     const closeAboutPopup = () => {
         setShowAboutPopup(false);
     };
@@ -23,21 +30,24 @@ export default function DesktopUI() {
         <Box className="desktop-ui-container">
             <Box className="content-container">
                 <img src={hello} alt="hello svg" className="hello-image" />
+                {showContacts && <Contacts />}
+                {showProjects && <Projects />}
+                {showSkills && <Skills />}
                 <Box className="buttons-container">
                     <Box className="column">
-                        <Box className="icon-box">
+                        <Box className="icon-box" onClick={() => setShowContacts(true)}>
                             <img src={contactsIcon} alt="contact icon" className="icon" />
                             <Typography variant="h5" className="icon-text" fontFamily="var(--secondary-font)" fontWeight="600" >
                                 Contact.me
                             </Typography>
                         </Box>
-                        <Box className="icon-box">
+                        <Box className="icon-box" onClick={() => setShowProjects(true)}>
                             <img src={projectsIcon} alt="projects icon" className="icon" />
                             <Typography variant="h5" className="icon-text" fontFamily="var(--secondary-font)" fontWeight="600" >
                                 My-Projects.exe
                             </Typography>
                         </Box>
-                        <Box className="icon-box">
+                        <Box className="icon-box" onClick={() => setShowSkills(true)}>
                             <img src={skillsIcon} alt="skills icon" className="icon" />
                             <Typography variant="h5" className="icon-text" fontFamily="var(--secondary-font)" fontWeight="600" >
                                 My-skills.rar
