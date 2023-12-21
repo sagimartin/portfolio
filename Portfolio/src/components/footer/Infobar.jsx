fed90f050e07bbac661b8adb644905095a27da00
Martin Sági <sagimrtin@gmail.com>
2023-12-21 09:03:48 +0100

    Deactivate BatteryStatus

--- Portfolio/src/components/footer/Infobar.jsx
+++ Portfolio/src/components/footer/Infobar.jsx
@@ -2,16 +2,16 @@ import { Box } from "@mui/material";
 import Weather from "./Weather";
 import Clock from "./Clock";
 import wifi from "/assets/icons/wifi.svg"
-import BatteryStatus from "./BatteryStatus";
+// import BatteryStatus from "./BatteryStatus";
 
 
 export default function Infobar() {
     return (
         <Box className="infobar-container" style={{ color: "white", border: "3px inset #fff", margin: ".5rem", padding: ".25rem .5rem", display: 'flex', alignItems: "center", gap: ".5rem" }}>
             <Weather />
-            <Clock />
             <img className="wifi-logo" src={wifi} alt="wifi logo" style={{ height: "2.5rem" }} />
-            <BatteryStatus/>
+            <Clock />
+            {/* <BatteryStatus/> */}
         </Box>
     )
 }
