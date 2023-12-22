041c5e73b5572f5469805ea1eefdd59f2291db7d
Martin Sági <sagimrtin@gmail.com>
2023-12-22 17:35:09 +0100

    Switch to 24-hour format

--- Portfolio/src/components/footer/Clock.jsx
+++ Portfolio/src/components/footer/Clock.jsx
@@ -10,22 +10,20 @@ export default function Clock() {
             let h = date.getHours();
             let m = date.getMinutes();
             let s = date.getSeconds();
-            let session = "AM";
 
             if (h === 0) {
-                h = 12;
+                h = 24;
             }
 
-            if (h > 12) {
-                h = h - 12;
-                session = "PM";
+            if (h > 24) {
+                h = h - 24;
             }
 
             h = (h < 10) ? "0" + h : h;
             m = (m < 10) ? "0" + m : m;
             s = (s < 10) ? "0" + s : s;
 
-            const currentTime = h + ":" + m + ":" + s + " " + session;
+            const currentTime = h + ":" + m + ":" + s + " ";
             setTime(currentTime);
         };
 
