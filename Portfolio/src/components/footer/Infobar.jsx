7c6057ebbbb2bde759bbb7905608ccda550ffc35
Martin Sági <sagimrtin@gmail.com>
2024-07-10 08:47:30 +0200

    Implement language switcher component with flag icons

--- Portfolio/src/components/footer/Infobar.jsx
+++ Portfolio/src/components/footer/Infobar.jsx
@@ -2,16 +2,19 @@ import { Box } from "@mui/material";
 import Weather from "./Weather";
 import Clock from "./Clock";
 import wifi from "/assets/icons/wifi.svg"
+import LanguageSwitcher from "./LanguageSwitcher";
 // import BatteryStatus from "./BatteryStatus";
 
 
 export default function Infobar() {
+
     return (
         <Box className="infobar-container" style={{ color: "white", border: "3px inset #fff", margin: ".5rem", padding: ".25rem .5rem", display: 'flex', alignItems: "center", gap: ".5rem" }}>
             <Weather />
             <img className="wifi-logo" src={wifi} alt="wifi logo" style={{ maxWidth: "2rem" }} />
             <Clock />
             {/* <BatteryStatus/> */}
+            <LanguageSwitcher />
         </Box>
     )
 }
---
+++ Portfolio/src/components/footer/LanguageSwitcher.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Box, IconButton, Tooltip, Menu, MenuItem } from "@mui/material";
+import { useTranslation } from "react-i18next";
+import ukFlag from "/assets/icons/uk-flag.svg";
+import hungaryFlag from "/assets/icons/hungary-flag.svg";
+
+export default function LanguageSwitcher() {
+    const { i18n } = useTranslation();
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        handleClose();
+    };
+
+    const currentLanguage = i18n.language;
+    const flag = currentLanguage === 'en' ? ukFlag : hungaryFlag;
+    const tooltipTitle = currentLanguage === 'en' ? 'ENG' : 'HUN';
+
+    return (
+        <Box>
+            <Tooltip title={tooltipTitle} arrow>
+                <IconButton onClick={handleClick}>
+                    <img src={flag} alt={tooltipTitle} style={{ width: '2rem', height: 'auto' }} />
+                </IconButton>
+            </Tooltip>
+            <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                sx={{
+                    '& .MuiPaper-root': {
+                        border: "3px inset #fff",
+                    }
+                }}
+            >
+                <MenuItem onClick={() => changeLanguage('en')}>
+                    <img src={ukFlag} alt="English" style={{ width: '2rem', height: 'auto', marginRight: '0.5rem' }} />
+                    English
+                </MenuItem>
+                <MenuItem onClick={() => changeLanguage('hu')}>
+                    <img src={hungaryFlag} alt="Hungarian" style={{ width: '2rem', height: 'auto', marginRight: '0.5rem' }} />
+                    Magyar
+                </MenuItem>
+            </Menu>
+        </Box>
+    );
+}
