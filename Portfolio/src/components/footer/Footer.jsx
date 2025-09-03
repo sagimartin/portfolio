29676b0f39fc69ca737ff5565ab686054b9e95a1
Martin Sági <sagimrtin@gmail.com>
2025-09-03 15:29:27 +0200

    Infobar CSS fix

--- Portfolio/src/components/footer/Footer.jsx
+++ Portfolio/src/components/footer/Footer.jsx
@@ -1,13 +1,31 @@
 import { Box } from "@mui/material";
 import Infobar from "./Infobar";
-import sm from "/assets/icons/sm.svg"
+import sm from "/assets/icons/sm.svg";
 
 export default function Footer() {
-    return (
-        <Box style={{ width: "100%", backgroundColor: "#c0c0c0", borderTop: "3px solid darkgray", padding: "0 .5rem", display: "flex", justifyContent: 'space-between', alignItems: 'center', cursor: "default" }}>
-            <Box><img className="start-icon" src={sm} style={{ width: "3rem", display: "flex", alignItems: "center" }} /></Box>
-            <Infobar />
-        </Box>
-    );
-}
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        backgroundColor: "#c0c0c0",
+        borderTop: "3px solid darkgray",
+        padding: "0 .5rem",
+        display: "flex",
+        alignItems: "center",
+        cursor: "default",
+      }}
+    >
+      <Box>
+        <img
+          className="start-icon"
+          src={sm}
+          style={{ width: "3rem", display: "flex", alignItems: "center" }}
+        />
+      </Box>
 
+      <Box sx={{ flexGrow: 1 }} />
+
+      <Infobar />
+    </Box>
+  );
+}
--- Portfolio/src/components/footer/Infobar.jsx
+++ Portfolio/src/components/footer/Infobar.jsx
@@ -1,30 +1,35 @@
 import { Box } from "@mui/material";
 import Weather from "./Weather";
 import Clock from "./Clock";
-import wifi from "/assets/icons/wifi.svg"
+import wifi from "/assets/icons/wifi.svg";
 // import LanguageSwitcher from "./LanguageSwitcher";
 // import BatteryStatus from "./BatteryStatus";
 
-
 export default function Infobar() {
-
-    return (
-        <Box className="infobar-container" style={{
-            color: "white",
-            border: "3px inset #fff",
-            margin: ".5rem",
-            padding: ".25rem .5rem",
-            display: 'inline-flex',
-            alignItems: "center",
-            gap: ".5rem",
-            maxWidth: "50vw",
-            overflow: "hidden"
-        }}>
-            <Weather />
-            <img className="wifi-logo" src={wifi} alt="wifi logo" style={{ maxWidth: "2rem" }} />
-            <Clock />
-            {/* <BatteryStatus/> */}
-            {/* <LanguageSwitcher /> */}
-        </Box>
-    )
+  return (
+    <Box
+      className="infobar-container"
+      sx={{
+        color: "white",
+        border: "3px inset #fff",
+        margin: ".5rem",
+        padding: ".25rem .5rem",
+        display: "inline-flex",   
+        alignItems: "center",
+        gap: ".5rem",
+        whiteSpace: "nowrap",     
+      }}
+    >
+      <Weather />
+      <img
+        className="wifi-logo"
+        src={wifi}
+        alt="wifi logo"
+        style={{ maxWidth: "2rem" }}
+      />
+      <Clock />
+      {/* <BatteryStatus /> */}
+      {/* <LanguageSwitcher /> */}
+    </Box>
+  );
 }
